11eb4e4bef1bb806bde849eee938d0df84519424
markerpen <markchou920219@gmail.com>
2024-08-14 03:05:07 -0700

    test-0error-0814

--- self-modules/datatype/datatype.h
+++ self-modules/datatype/datatype.h
@@ -2,16 +2,12 @@
 #ifndef DATATYPE_H
 #define DATATYPE_H
 
-
 #include <iostream>
 #include <map>
 #include <initializer_list>
 #include <string>
 
-
 namespace ns3 {
-
-/* ... */
 
     template<typename K, typename V>
     class Dict {
@@ -24,9 +20,43 @@ namespace ns3 {
             return dict[key];
         }
 
+        // Add or update element
+        void set(const K& key, const V& value) {
+            dict[key] = value;
+        }
+
+        // Remove element by key
+        bool remove(const K& key) {
+            auto it = dict.find(key);
+            if (it != dict.end()) {
+                dict.erase(it);
+                return true;
+            }
+            return false;
+        }
+
         // Find element
         typename std::map<K, V>::iterator find(const K& key) {
             return dict.find(key);
+        }
+
+        typename std::map<K, V>::const_iterator find(const K& key) const {
+            return dict.find(key);
+        }
+
+        // Check if element exists
+        bool contains(const K& key) const {
+            return dict.find(key) != dict.end();
+        }
+
+        // Get size of the dictionary
+        size_t size() const {
+            return dict.size();
+        }
+
+        // Clear all elements
+        void clear() {
+            dict.clear();
         }
 
         // Print all elements
@@ -55,7 +85,14 @@ namespace ns3 {
         return Dict<K, V>(init);
     }
 
+    struct value
+    {
+        std::string  Key  ;
+        int          INT  ;
+        float       FLOAT ;
+        double      DOUBLE;
+        std::string STRING;
+    };
 }
 
 #endif /* DATATYPE_H */
-
